8ee0c623b8fcf4ae7104b9a0a7c4c6d9845ccb41
Maalinishree Elango <maalinishree.invts@Ravikumar-N.local>
2020-08-09 22:28:27 +0530

    Added Footer

---
+++ src/Dashboard /Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from "../Image/download.jpeg";
+function Header() {
+  return (
+    <div style={{background: "#E5E5E5", height: "100%"}}>
+    </div>
+  );
+}
+
+export default Header;
